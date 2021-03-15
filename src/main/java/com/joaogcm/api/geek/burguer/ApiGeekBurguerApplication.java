164accc0206ca2cc536a2e826375217364f38563
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2021-03-15 11:09:37 -0300

    API Criada

---
+++ src/main/java/com/joaogcm/api/geek/burguer/ApiGeekBurguerApplication.java
@@ -0,0 +1,11 @@
+package com.joaogcm.api.geek.burguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeekBurguerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeekBurguerApplication.class, args);
+	}
+}
